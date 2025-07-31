0068769c2919b4d8792b387d6da0a72da6f39bf9
alanweb7 <alanweb7@gmail.com>
2025-07-31 00:06:15 -0300

    Remoção do módulo Advanced Tools

--- modules/advanced_tools/language/portuguese_br/advanced_tools_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['advanced_tools'] = 'Advanced Tools';
-$lang['Nome do Módulo'] = 'Nome do Módulo';
-$lang['Menu Principal'] = 'Menu Principal';
-$lang['Autor'] = 'Autor';
-$lang['submit'] = 'Salvar';
