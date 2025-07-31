edc5c4b41ebdbf20e67e01aa87189c3998402ff9
alanweb7 <alanweb7@gmail.com>
2025-07-31 00:07:24 -0300

    Remoção do módulo Advanced Tools

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if( ! ini_get('date.timezone') )
 }
 /**
  * CodeIgniter
- * in production 3
+ *
  * An open source application development framework for PHP
  *
  * This content is released under the MIT License (MIT)
