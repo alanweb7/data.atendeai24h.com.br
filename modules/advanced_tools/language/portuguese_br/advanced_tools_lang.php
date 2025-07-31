98c2a2c58f1331923c8692f9e16c36214fb20a3e
alanweb7 <alanweb7@gmail.com>
2025-07-30 23:56:32 -0300

    Instalação do módulo Advanced Tools

---
+++ modules/advanced_tools/language/portuguese_br/advanced_tools_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['advanced_tools'] = 'Advanced Tools';
+$lang['Nome do Módulo'] = 'Nome do Módulo';
+$lang['Menu Principal'] = 'Menu Principal';
+$lang['Autor'] = 'Autor';
+$lang['submit'] = 'Salvar';
